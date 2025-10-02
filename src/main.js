8b4c21c37a38a2458fa807fd77cd657be65f6c88
Falkort <lleclercq91@outlook.com>
2025-10-02 11:54:36 +0200

    Mise en place des components

--- src/main.js
+++ src/main.js
@@ -1,24 +1,69 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+class DB{
+  //2. Recupération de l'url via le data
+  static setApiURL(data){
+    this.apiURL = data;
+  }
+  static async findAll(){
+    //3. on peut utiliser apiURL grâce à setApiURL
+    const response = await fetch(this.apiURL + "contacts");
+    return response.json();
+  }
+}
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function getContactTemplate(contact){
+  return `
+    <li>${contact.firstname} / ${contact.lastname} / ${contact.email}</li>
+    `;
+}
 
-setupCounter(document.querySelector('#counter'))
+class Contact{
+  constructor(data){
+    this.id=data.id;
+    this.firstname=data.firstname;
+    this.lastname=data.lastname;
+    this.email=data.email;
+    this.createdAt=data.createdAt;
+  }
+  render(el){
+    const template = document.createElement('div');
+    template.innerHTML = getContactTemplate(this);
+    el.append(template);
+  }
+}
+
+function getContactListTemplate(){
+  return `
+     <h1> Contact List</h1>
+     <ul class=contact-list></ul>
+     <p>MA super liste de contact</p>
+     `
+}
+
+class ContactList{
+  constructor(data){
+    // Permet de prendre le focus sur #app via data.el et le el de new ContactList
+    this.domElt=document.querySelector(data.el);
+    // 1. La fonction setApiURL vas récupérer apiURL à travers data
+    DB.setApiURL(data.apiURL);
+    //On initialise les contacts sur un tableau vide
+    this.contacts =[];
+    //On charge les contacts
+    this.loadContacts();
+  }
+  async loadContacts(){
+    const contacts= await DB.findAll();
+    this.contacts=contacts.map(contact => new Contact(contact));
+    this.render();
+  }
+  render(){
+     this.domElt.innerHTML = getContactListTemplate();
+     this.contacts.forEach(contact => contact.render(this.domElt.querySelector(".contact-list")));
+  }
+}
+
+
+
+new ContactList({
+  el:"#app",
+  apiURL:"https://68dd1ef87cd1948060ac7fc2.mockapi.io/"
+});
