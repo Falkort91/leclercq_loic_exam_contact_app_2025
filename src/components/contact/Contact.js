f2da2973098fac41e5e6837624d5e36a96c14930
Falkort <lleclercq91@outlook.com>
2025-10-03 16:16:31 +0200

    Intégration du template des components

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,10 +7,12 @@ export default class Contact{
     this.lastname=data.lastname;
     this.email=data.email;
     this.createdAt=data.createdAt;
+    this.domElt=null;
   }
   render(el){
-    const template = document.createElement('div');
+    const template = document.createElement('template');
     template.innerHTML = getTemplate(this);
-    el.append(template);
+    this.domElt=template.content.firstElementChild;
+    el.append(this.domElt);
   }
 }
